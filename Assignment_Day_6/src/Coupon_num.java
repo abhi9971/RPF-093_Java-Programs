8b2849edea261c17335f5ec73592fcd1db17bec2
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-01 18:54:32 +0530

    Function Programs

--- Assignment_Day_6/src/Coupon_num.java
+++ Assignment_Day_6/src/Coupon_num.java
@@ -21,9 +21,11 @@ public static int getCoupon(int n) {
 	    
 	    public static void main(String[] args)
 	    {
+		    
 	        int n = Integer.parseInt(args[0]);
 	        int count = collect(n);
 	        System.out.println(count);
+		    
 	    }  
 
 	}
