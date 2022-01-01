dfd6c3c1e55393081abc12f3772b698348b48dd0
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-01 17:51:08 +0530

    Update TakeMoreThanThreeParameter.java

--- Assignment_Day_13/FindMaxUsingGenerics/TakeMoreThanThreeParameter.java
+++ Assignment_Day_13/FindMaxUsingGenerics/TakeMoreThanThreeParameter.java
@@ -1,16 +1,21 @@
 public class TakeMoreThanThreeParameter<T extends Comparable<T>> {
+    
     public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
+        
         T max = val1;
         if (val2.compareTo(max) > 0 ) {
             max = val2;
-        } if (val3.compareTo(max) > 0){
+        } 
+        if (val3.compareTo(max) > 0){
             max = val3;
-        }if (val4.compareTo(max) > 0 ){
-            max = val4;
-        }if (val5.compareTo(max) > 0){
-            max = val5;
-
         }
+        if (val4.compareTo(max) > 0 ){
+            max = val4;
+        }
+        if (val5.compareTo(max) > 0){
+            max = val5;
+        }
+        
         return max;
     }
 
