6e47342a6709ef6628e0116530ea461468b5c542
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-02 13:02:07 +0530

    Update JSONInventory.java

--- Assignment_Day_11-12/OopsPracticeProblems/Inventory Management/JSONInventory.java
+++ Assignment_Day_11-12/OopsPracticeProblems/Inventory Management/JSONInventory.java
@@ -1,12 +1,19 @@
 import org.json.simple.JSONArray;
+
 import org.json.simple.JSONObject;
+
 import org.json.simple.parser.JSONParser;
+
 import org.json.simple.parser.ParseException;
 
 import java.io.FileNotFoundException;
+
 import java.io.FileReader;
+
 import java.io.FileWriter;
+
 import java.io.IOException;
+
 import java.util.Scanner;
 
 public class JSONInventory {
@@ -68,8 +75,8 @@ public class JSONInventory {
             e.printStackTrace();
         } 
         catch (ParseException e) {
-            e.printStackTrace();
             
+            e.printStackTrace();           
         }
     }
 
