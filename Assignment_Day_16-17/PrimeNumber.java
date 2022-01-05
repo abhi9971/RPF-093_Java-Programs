4911a1263e3e8b986058113fc381242a41f0bbbc
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-05 17:46:21 +0530

    Update PrimeNumber.java

--- Assignment_Day_16-17/PrimeNumber.java
+++ Assignment_Day_16-17/PrimeNumber.java
@@ -1,9 +1,14 @@
 public class PrimeNumber {
 
 	public static void main(String[] args) {
+		
 		Utility u = new Utility();
+		
 		System.out.println("Enter the number:");
+		
 	    int x = u.inputInteger();
+		
 	    System.out.println(u.primeNumber(x));
+		
 	}
 }
