f2d6f1c8ce4c4fa74366cd290220e06b028eab7f
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-05 16:28:26 +0530

    Update CreateLinkedList.java

--- Assignment_Day_14/Linked List/Use Cases Of Linked List/CreateLinkedList.java
+++ Assignment_Day_14/Linked List/Use Cases Of Linked List/CreateLinkedList.java
@@ -5,20 +5,30 @@ public class CreateLinkedList {
     public static class Node {
 
         private int data;
+        
         private Node next;
 
         public Node(int data) {
+            
             this.data = data;
+            
             this.next = null;
         }
 
         public static void main(String[] args) {
+            
             CreateLinkedList linkedList = new CreateLinkedList();
+            
             CreateLinkedList.head = new Node(56);
+            
             Node secondNode = new Node(30);
+            
             Node thirdNode = new Node(70);
+            
             linkedList.head.next = secondNode;
+            
             secondNode.next = thirdNode;
+            
             printList();
         }
     }
@@ -31,9 +41,12 @@ public class CreateLinkedList {
         System.out.print("LinkedList: ");
 
         while (currNode != null) {
+            
             System.out.print(currNode.data + "-->");
+            
             currNode = currNode.next;
         }
+        
         System.out.println(currNode);
     }
 }
