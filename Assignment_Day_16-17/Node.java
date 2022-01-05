4199e229cdee0d20ccdafb74c1b9bb8c09c7db0f
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-05 17:37:58 +0530

    Update Node.java

--- Assignment_Day_16-17/Node.java
+++ Assignment_Day_16-17/Node.java
@@ -1,12 +1,15 @@
 class Node {
 
     public Node(Object data) {
+        
         super();
+        
         this.data = data;
+        
         this.next = null;
     }
 
-
     Object data;
+    
     Node next;
 }
