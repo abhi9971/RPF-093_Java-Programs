03aa31af474961300638811c2a20044115d7f70a
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-05 17:23:28 +0530

    Update FindNumber.java

--- Assignment_Day_16-17/FindNumber.java
+++ Assignment_Day_16-17/FindNumber.java
@@ -1,14 +1,25 @@
 public class FindNumber {
+	
 	public static String s,f,t,r;
+	
 	public static void main(String[] args) {
+		
 		int choice = 0;
-		do{
+		
+		do {
+			
 			System.out.println("Enter the Range  (N) :");
+			
 			Utility u= new Utility();
+			
 			int num =u.inputInteger();
+			
 			u.find(0,num-1);
+			
 			System.out.println("press 1 to continue");
+			
 			choice = u.inputInteger();
-		}while(choice == 1);	
+		} 
+		while(choice == 1);	
 	}
 }
