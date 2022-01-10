a932b19b63c66d1a1041dabc8fe29df3b10c9415
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-10 16:04:15 +0530

    Add files via upload

---
+++ Assignment_Day_21/Mood Analyser/MoodAnalyzer.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.exception;
+
+public class MoodAnalyzer {
+
+    String message;
+
+    public MoodAnalyzer(String message) {
+
+        this.message = message;
+    }
+
+    public String analyzeMood(String message) throws MoodAnalysisException {
+
+        this.message = message;
+
+        return analyzeMood();
+
+    }
+
+    public String analyzeMood() throws MoodAnalysisException {
+
+        try {
+
+            if (message.length() == 0) {
+
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, "please enter proper message");
+
+            }
+
+            if (message.contains("sad")) {
+
+                return "SAD";
+
+            }
+
+            return "HAPPY";
+        }
+
+        catch (NullPointerException e) {
+
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL, "please enter proper message");
+
+        }
+    }
+}
