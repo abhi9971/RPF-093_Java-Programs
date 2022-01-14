ed1eafbebf11342482bb5b8bb973c301c08d49b0
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-14 11:05:42 +0530

    Implementation of Junit is done

---
+++ Assignment_Day_20/Use Cases/UserRegistrationInterface.java
@@ -0,0 +1,6 @@
+package com.briddgelabz.Junit;
+@FunctionalInterface
+public interface UserRegistrationInterface {
+    public abstract boolean userEntries(String value) throws InvalidUserInputException;
+}
+
